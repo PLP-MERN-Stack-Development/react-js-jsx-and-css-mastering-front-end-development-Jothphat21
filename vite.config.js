eab2bc6d4ce252409a2ad0935aa2be5a759700e0
Josephat <josephatmwoleka@gmail.com>
2025-10-23 09:03:08 +0300

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-js-jsx-and-css-mastering-front-end-development/",
+  base: "/react-js-jsx-and-css-mastering-front-end-development-Jothphat21/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
